2ad71da7774c3526c0e1830d8ad8b376ac0a6dba
philupa <philippaclarkson@gmail.com>
2023-10-13 09:25:57 +0100

    tidied up

--- trial.py
+++ trial.py
@@ -3,12 +3,12 @@ digits = datasets.load_digits()
 import matplotlib.pyplot as plt
 
 clf = svm.SVC(gamma=0.001, C=100.)
-fit_method = clf.fit(digits.data[:-2], digits.target[:-2])
+fit_method = clf.fit(digits.data[:-1], digits.target[:-1])
 
-prediction = clf.predict(digits.data[-2:])
+prediction = clf.predict(digits.data[-1:])
 print(prediction)
 
 plt.figure(1, figsize=(3, 3))
-plt.imshow(digits.images[-2], cmap=plt.cm.gray_r, interpolation="nearest")
+plt.imshow(digits.images[-1], cmap=plt.cm.gray_r, interpolation="nearest")
 plt.show()
 
